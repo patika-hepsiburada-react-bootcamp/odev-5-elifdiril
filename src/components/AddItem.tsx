cd7d16b5e643f4bb7a34d629ff3a3f5d48cb6713
elif <elifdiril6@gmail.com>
2021-10-18 17:20:07 +0300

    toggle all added

--- src/components/AddItem.tsx
+++ src/components/AddItem.tsx
@@ -5,8 +5,11 @@ import TodoItem from '../iterfaces/TodoItem';
 const AddItem: React.FC<IProps> = ({ list, setList }: IProps) => {
     const [item, setItem] = useState<string>('');
     const [counter, setCounter] = useState<number>(2);
+    const [toggleAll, setToggleAll] = useState<boolean>(false);
 
-    const addNewItem = () => {
+    const addNewItem = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+
         if (item !== "") {
             const newTodos: TodoItem[] = [...list, { id: counter + 1, todo: item, status: false }];
             setList(newTodos);
@@ -19,10 +22,28 @@ const AddItem: React.FC<IProps> = ({ list, setList }: IProps) => {
         setItem(e.target.value);
     };
 
+    const handleToggleAll = () => {
+        const newList = list.map((item: TodoItem) => {
+            const updatedItem = {
+                ...item,
+                status: !toggleAll,
+            };
+
+            return updatedItem;
+        });
+        setList(newList);
+        setToggleAll(!toggleAll)
+    }
+
     return (
         <div className="new-todo">
-            <input value={item} onChange={handleChange} autoFocus placeholder="What needs to be done?" />
-            <button onClick={addNewItem}>Add</button>
+            <form onSubmit={addNewItem} autoComplete="off">
+                {list.length > 0 && <label className="toggle-all-label" >
+                    <input className="toggle-all" type="checkbox" checked={toggleAll} onChange={handleToggleAll} />
+                    Mark all as complete
+                </label>}
+                <input value={item} onChange={handleChange} autoFocus placeholder="What needs to be done?" />
+            </form>
         </div>
     );
 };
